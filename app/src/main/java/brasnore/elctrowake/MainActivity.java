f999d5987b72a21578ab9d695b497e29eb6274ae
tamir <tamir>
2018-04-25 00:38:04 +0300

    Added a lot of code from the internet, not really working

--- app/src/main/java/brasnore/elctrowake/MainActivity.java
+++ app/src/main/java/brasnore/elctrowake/MainActivity.java
@@ -1,12 +1,22 @@
 package brasnore.elctrowake;
 
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 
+@TargetApi(20)
 public class MainActivity extends AppCompatActivity {
+
+    static public final int REQUEST_ENABLE_BT = 3;
+
     public static final String EXTRA_MESSAGE = "LOL";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void bluetooth(View view){
-        // TODO: what ever the fuck is written here https://developer.android.com/guide/topics/connectivity/bluetooth-le.html
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+        }
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        final BluetoothAdapter mBluetoothAdapter = bluetoothManager.getAdapter();
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
 }
